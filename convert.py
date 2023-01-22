ebf406b753b5aa501ab52df2fb7266744d382803
dvarela76 <105849276+dvarela76@users.noreply.github.com>
2023-01-22 15:49:25 +0100

    ask 1 done

--- convert.py
+++ convert.py
@@ -20,10 +20,12 @@ def celsius_to_fahrenheit(t):
 options = ['c', 'f', 'q']
 
 for ask in options:
-    ask = input('What unit would you like to convert to? (Type c or f) ') #4. program asks what the user wants
+    ask = input('What unit would you like to convert to? (Type c, f or k; or q to quit) ') #4. program asks what the user wants
+    ask1 = input('What unit are you converting from? (Type c, f or k) ')
     if ask == 'c':
         def fahrenheit_to_celsius(t):
-            t_celsius = (t_fahrenheit-32)*(5/9) #2. fahrenheit_to_celsius fixed
+            t_celsius_f = (t_fahrenheit-32)*(5/9) #2. fahrenheit_to_celsius fixed
+            t_celsius_k = (t_fahrenheit-32)*(5/9)
             return t_celsius
         answer = input('Enter a temperature in Fahrenheit: ')
         t_fahrenheit = float(answer)
@@ -40,6 +42,6 @@ for ask in options:
         t = celsius_to_fahrenheit(t_celsius)
         print("Fahrenheit: ", t)
     elif ask == 'q':
-       quit
-    elif ask != 'q' or ask!= 'c' or ask!= 'f':
+       quit #5. continues until user quits
+    elif ask != 'q' or ask!= 'c' or ask!= 'f' or ask != 'k' or ask1!= 'c' or ask1!= 'f' or ask1!= 'k':
         print('Please input a valid option')
