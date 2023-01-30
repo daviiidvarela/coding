93ddbd6cb7f0790d65bc2a6c20b5f67e0cc4715c
dvarela76 <105849276+dvarela76@users.noreply.github.com>
2023-01-30 11:59:38 +0100

    new doc

--- labb2.py
+++ labb2.py
@@ -4,6 +4,8 @@ p0 = [2,0,1,0]
 q0 = [0,0,0]
 
 
+
+
 def poly_to_string(p_list):
     '''
     Return a string with a nice readable version of the polynomial given in p_list.
@@ -28,6 +30,7 @@ def poly_to_string(p_list):
             terms.append(term)
         if coeff == 0:
             term = str()
+            return str()
         degree += 1
         if (len(p_list)==0): 
 		        return 0 
@@ -37,8 +40,7 @@ def poly_to_string(p_list):
 
 def drop_zeroes(p_list):
             while p_list[-1] == 0:
-                p_list.pop()
-            return p_list
+                return p_list.pop()
 
 def eq_poly(p_list, q_list):
     if p_list == q_list:
@@ -46,9 +48,13 @@ def eq_poly(p_list, q_list):
     else:
         return False
 
+#def eval_poly(p_list, x):
+#    return coeff * x ** degree
 
 
-#print(poly_to_string(p))
+
+print(poly_to_string(p))
 #print(poly_to_string(q))
-print(eq_poly(q0,[]))
+#print(eq_poly(p,p0))
 #print(drop_zeroes(q0))
+#eval_poly(p0, 1)
