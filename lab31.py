81b5ff4e7fb6e37c7cecbd48bbe6e92c7c3e875b
dvarela76 <105849276+dvarela76@users.noreply.github.com>
2023-02-02 16:02:46 +0100

    taskb

--- lab31.py
+++ lab31.py
@@ -11,10 +11,22 @@ def string_to_seconds(t): # function to convert mm:ss to ss
 ## A ##
 def read_library(file):
     lib = open(file, 'r')
-    print(lib.read())
+    #print(lib.read())
     lib.close()
     
 #Test for task 2.a
 # read_library('80s_library.txt')
 
 ## B ##
+#
+
+def print_library(file):
+    lib = open('80s_library.txt', 'r')
+    for s in lib:
+        artist, sname, time = s.split(',')
+        for artist, sname, time in lib.items:
+            print(artist + ': ' + sname + ' (' + time + ')')
+    lib.close()
+
+lib = read_library('80s_library.txt')
+print_library(lib)
