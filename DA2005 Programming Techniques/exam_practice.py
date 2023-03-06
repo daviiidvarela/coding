4fce464dbdfd81526b1b6dfb34f4679ec201e3e5
daviiidvarela <davidfrancois.daluzvarela@gmail.com>
2023-03-06 21:46:08 +0100

    exam

---
+++ DA2005 Programming Techniques/exam_practice.py
@@ -0,0 +1,22 @@
+# s = 'DA2004'
+# print({ x : s.count(x) for x in s })
+
+# s = "DA2004"
+# out = ""
+# for x in s:
+#     try:
+#         for y in range(int(x)):
+#             out += x
+#     except:
+#         out += "X"
+# print(out)
+
+# def foo(mylist):
+#     m = 0
+#     while mylist:
+#         x = mylist.pop()
+#         if x > m:
+#             m = x
+#     return m
+# lista = [4,3,1,2,5,0]
+# print(foo(lista))
