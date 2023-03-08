d7f5328c63ae438a50cb5c1a670f5a625e9b7676
dvarela76 <105849276+dvarela76@users.noreply.github.com>
2023-03-08 10:23:46 +0100

    hh

---
+++ DA2005 Programming Techniques/Exam practice/Exam_12_01_23.py
@@ -0,0 +1,22 @@
+## Multiple Choice
+# 1. A, C, D c is correct
+
+# 2. A correct
+
+# 3. A, B, C, D, E a and c are correct
+
+# 4. B
+# x and y and (not y or not z) == True
+
+# True and True and (not True or not True)
+# True and False and (not False or not True)
+# True and True and (not True or not False)
+
+# 5. B d is correct
+
+# 6. 
+
+# 7.
+# 8. ABCD d is not
+# 9. ABCDE bc not
+# 10. DE correct
