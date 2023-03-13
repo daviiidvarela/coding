eb992d7455c527b35076bee1a258578f8d1af7d6
dvarela76 <105849276+dvarela76@users.noreply.github.com>
2023-03-13 13:23:29 +0100

    lots of work

--- R code/Time series.R
+++ R code/Time series.R
@@ -2,36 +2,35 @@
 ##install.packages("tseries")
 ##install.packages("lmtest")
 
-
 library(forecast)
 library(tseries)
 library(lmtest)
 
-
-
-
-
 BLK
 
-BLK <- BLK[,-c(3,4)]
+#BLK <- BLK[,-c(3,4)]
 
-BLK$logclose <-log(BLK$Close)
+BLK$logclose <- log(BLK$Close)
 
-BLK$logreturn <-c(NA,diff(BLK$logclose))
+BLK$logreturn <- c(NA,diff(BLK$logclose))
 
-BLK$log_Adj.close <-log(BLK$Adj.Close)
+BLK$log_Adj.close <- log(BLK$Adj.Close)
 
-BLK$logreturn_Adj.close <-c(NA, diff(BLK$log_Adj.close))
-
-
+BLK$logreturn_Adj.close <- c(NA, diff(BLK$log_Adj.close))
 
 
 
-BLK$Close <-ts(BLK$Close, start =c(2017, 01), frequency = 12)
+
+
+BLK$Close <-ts(BLK$Close, start =c(2017, 01), frequency = 12, end =c(2022, 12))
 BLK$logreturn <-ts(BLK$logreturn, start =c(2017, 01), frequency = 12)
-ts.plot(BLK$Close ,  main = "close vs time" ,  ylab = "close" )
+ts.plot(BLK$Close ,  main = "Closing price of BlackRock Inc (BLK) vs Time" ,  ylab = "Closing price ($USD)" )
 
-ts.plot(BLK$logreturn ,  main = "log return vs time" ,  ylab = "log return" )
+ggmonthplot(x=BLK, labels = NULL, times = time(x=BLK), phase = cycle(x=BLK))
+
+ggsubseriesplot(x=BLK, labels = NULL, times = time(x=BLK), phase = cycle(x=BLK))
+
+ts.plot(BLK$logreturn ,  main = "Log return of BlackRock Inc (BLK) vs Time" ,  ylab = "Log return" )
 abline(h = 0, lty = 2 , col = "dark red" )
 
 training <- BLK[1:(nrow(BLK)-4), ]
@@ -47,3 +46,4 @@ plot(holt_model)
 lines((nrow(BLK)-3):nrow(BLK), testing$Close,col = "dark red", lty = 1, lwd = 2, type = "o")
 
 adf.test(BLK$logreturn_Adj.close[-1], k =1)
+
